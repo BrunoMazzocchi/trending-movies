1e94b0a94e2793bc59d7e1072d7c8b09e1cfbc00
Bruno Mazzocchi <brunomazzocchidev@gmail.com>
2024-06-27 20:44:43 -0600

    Added folder structure

---
+++ MovieTrending/Controllers/MainViewController/MainViewController.swift
@@ -0,0 +1,31 @@
+//
+//  MainViewController.swift
+//  MovieTrending
+//
+//  Created by Bruno Mazzocchi on 27/6/24.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.title = "Main view"
+        
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
